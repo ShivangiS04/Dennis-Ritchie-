a96bcceca38a2d647e8fd26465ec6ebb2daaeddb
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-05-25 11:44:11 +0530

    Add files via upload

---
+++ For statement.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*print Fahrenheit-celsuis table*/
+main()
+{
+    int fahr;
+
+    for (fahr=0;fahr<=300;fahr=fahr+20)
+    printf("%3d %6d.1f\n", fahr,(5.0/9.0)*(fahr-32));
+}
